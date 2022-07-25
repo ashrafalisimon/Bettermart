079f5bd61841ff9c864880a145d87394570b6155
Ashraf Ali <ashrafalicitp@gmail.com>
2022-07-26 01:28:32 +0600

    user auth and contorler done

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 const ErrorHandler = require('./middleware/error');
+const cookieParser = require('cookie-parser');
 
 app.use(express.json());
-
+app.use(cookieParser());
 // route import
 const product =require('./routes/ProductRoute');
+const user =require('./routes/UserRoute');
 
 app.use('/api/v2',product);
+app.use('/api/v2',user);
+
 app.use(ErrorHandler);
 
 module.exports = app;
